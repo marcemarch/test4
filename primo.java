4563dfeb3462d13a672c6245165e5d5b4a0bdaee
german <march.aruquipa@gmail.com>
2026-03-15 19:41:27 -0400

    algoritmo de verificar primo mejorado

--- primo.java
+++ primo.java
@@ -15,6 +15,18 @@ public class Primo1 {
         return true;
     }
 
+    public static boolean esPrimo2(int n) {
+        if (n <= 1) return false;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
 
         int numero = 17;
@@ -25,5 +37,10 @@ public class Primo1 {
             System.out.println("No es primo");
         }
 
+        if (esPrimo2(numero)) {
+            System.out.println("Es primo");
+        } else {
+            System.out.println("No es primo");
+        }
     }
 }
